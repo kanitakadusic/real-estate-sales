8cc517918de21a3efc1acfa9d60919afa860012e
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2025-01-06 12:07:06 +0100

    Page 'Moji upiti' done

---
+++ public/js/mojiUpiti.js
@@ -0,0 +1,31 @@
+function showUserQueries() {
+    const queriesElement = document.getElementById('user-queries');
+
+    PoziviAjax.getMojiUpiti((error, queries) => {
+        if (error) {
+            window.alert(`${error.status}: ${error.statusText}`);
+            return;
+        }
+
+        queries.forEach(query => {
+            const { id_nekretnine, tekst_upita } = query;
+
+            PoziviAjax.getNekretnina(id_nekretnine, (error, property) => {
+                if (error) {
+                    window.alert(`${error.status}: ${error.statusText}`);
+                    return;
+                }
+
+                const queryElement = document.createElement('li');
+                queryElement.innerHTML = `
+                    <strong>${property.naziv}</strong> [${property.datum_objave}] (${id_nekretnine})<br>
+                    ${tekst_upita}
+                `;
+
+                queriesElement.appendChild(queryElement);
+            });
+        });
+    });
+}
+
+showUserQueries();
